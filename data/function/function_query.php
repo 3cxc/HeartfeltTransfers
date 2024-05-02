3a9fd80b54b55f889448bbc7c88379f27e4a0f3f
3cxc <81449257+3cxc@users.noreply.github.com>
2024-05-02 23:23:12 +0800

    修复无法正确解析服务器URL的问题
    Signed-off-by: 3cxc <81449257+3cxc@users.noreply.github.com>

--- data/function/function_query.php
+++ data/function/function_query.php
@@ -1,11 +1,9 @@
 <?php
-  #获取url
+  # 获取到url,并且进行url解码
   function queryUrl(){
-     # 获取到url,并且进行url解码
-     $url = urldecode('https://'.$_SERVER['SERVER_NAME'].':'.$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"]);
-     return $url;
+     return urldecode('https://'.$_SERVER['HTTP_HOST'].$_SERVER["REQUEST_URI"]);
   }
-  #解析url的参数
+  # 解析url的参数
   function parseUrl($url){
     if(filter_var($url, FILTER_VALIDATE_URL) !== false){ #检查类型
       return parse_url($url);
