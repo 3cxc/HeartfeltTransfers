ac90a3609019725f2ffc4265f33bb8c4f26fd438
3cxc <81449257+3cxc@users.noreply.github.com>
2024-08-01 12:47:11 +0800

    New configuration file
    Signed-off-by: 3cxc <81449257+3cxc@users.noreply.github.com>

---
+++ config/config_data.php
@@ -0,0 +1,14 @@
+<?php
+    #下载配置文件
+    #Download configuration file
+
+    #文件下载设置
+    #请将要提供的下载文件上传到static/file目录(需要手动创建)
+    # File Download Settings
+    # Please upload the download file to the static/file directory
+    # which needs to be manually created
+   $download_array = array(
+      "example" => "example.exe",
+      "example1" => "example1.zip",
+   );
+?>
---
+++ config/config_global.php
@@ -0,0 +1,26 @@
+<?php
+   #全局配置文件
+   #Global configuration file
+
+   # 下载站地址
+   # Download site address
+   $App_Website = "https://example.com";
+
+   # 下载站所有者
+   # Download site owner
+   $API_Owner = "example.com";
+
+   # 下载站介绍
+   # Download site description
+   $API_Description = "example Download API";
+
+   # 是否启用维护模式(维护模式下无法下载文件)
+   # Is maintenance mode enabled 
+   # files cannot be downloaded in maintenance mode
+   $Anti_switch = false;
+
+   # 是否启用调试模式(仅开发者使用)
+   # Whether to enable debug mode (for developers only)
+   $Debug_mode = false;
+
+?>
