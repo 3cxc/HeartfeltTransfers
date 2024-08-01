a30a86532db725d2ed2d8b3ce4c83ebaa097694c
3cxc <81449257+3cxc@users.noreply.github.com>
2024-08-01 12:45:37 +0800

    Fix bugs
    Signed-off-by: 3cxc <81449257+3cxc@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
 <?php
-  #设置头
   header('Content-type: application/json; charset=utf-8"');
-  #导入处理json的php
+
   require_once('./data/function/function_json.php');
-  require_once('./config.php');
+  require_once('./config/config_global.php');
+
+  $Status = "OK";
+  $RunTime_Mode = "productionMode";
+  
+  if($Anti_switch){
+    $Status = "maintenanceMode";
+  }
+  if($Debug_mode){
+    $RunTime_Mode = "DebuggingMode";
+  }
 
   #格式化数组为json并输出
-  $json_arr = array("Status" => "OK","Runtime-Mode" => "productionMode","Application-Author" => "3cxc","Application-Description" => $API_Description,"Application-Version" => "3.2","Application-Owner" => $API_Owner);
+  $json_arr = array(
+    "Status" => $Status,
+    "Runtime-Mode" => $RunTime_Mode,
+    "Application-Author" => "3cxc",
+    "Application-Description" => $API_Description,
+    "Application-Version" => "3.3",
+    "Application-Owner" => $API_Owner);
   exit(formatJson($json_arr));
+
 ?>
