44d170da3927ce94df8334678279abbd0a1f1ba4
3cxc <81449257+3cxc@users.noreply.github.com>
2024-08-01 12:47:58 +0800

    Add maintenance function
    Signed-off-by: 3cxc <81449257+3cxc@users.noreply.github.com>

--- data/download.php
+++ data/download.php
@@ -1,16 +1,16 @@
 <?php
-   #用以下载游戏的php
-   #设置头
    header('Content-type: application/json; charset=utf-8"');
-   #导入处理json的php
-   include './function/function_json.php';
-   #导入url参数查询
-   include './function/function_query.php';
-   #导入遍历匹配函数
-   include './function/function_foreach.php';
-   #导入配置文件
-   include '../config.php';
+
+   require_once './function/function_json.php';
+   require_once './function/function_query.php';
+   require_once './function/function_foreach.php';
+   require_once '../config/config_data.php';
+   require_once '../config/config_global.php';
+
    #遍历匹配
+   if ($Anti_switch){
+      exit(http_response_code(403));
+   }
    exit(download(getTypeForeach(array_keys($download_array))));
 
 
