aa044f5323a4edab94edf0a07bc92600180a2a39
3cxc <81449257+3cxc@users.noreply.github.com>
2024-08-01 12:46:44 +0800

    Change layout
    Signed-off-by: 3cxc <81449257+3cxc@users.noreply.github.com>

--- static/common/502.php
+++ static/common/502.php
@@ -1,9 +1,8 @@
 <?php
-  #502
   header('Content-type: application/json; charset=utf-8"');
-  #导入处理json的php
   require_once('../../data/function/function_json.php');
+
   $json_str = '{"code":502,"reason":"Bad Gateway"}';
-  #设置返回的信息
+
   exit(formatJson($json_str));
 ?>
